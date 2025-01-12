7b354f6ff1bd6cdf19ee2769199679843d1738ce
Smithiboss <jamesbond.15091995@gmail.com>
2025-01-12 15:28:38 +0100

    Basic changes

--- src/main/Game.java
+++ src/main/Game.java
@@ -4,6 +4,7 @@ public class Game {
     // ...
     public Game() {
         GamePanel gamePanel = new GamePanel();
+        gamePanel.setup_game();
         gamePanel.startGameThread();
         GameWindow gameWindow = new GameWindow(gamePanel);
     }
